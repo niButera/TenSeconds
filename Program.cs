bd8bf680c84cd3981e04bf2108011577dd2b2d88
NiButera <nikolasxp683@gmail.com>
2023-03-19 02:07:48 -0300

    Fiz um programa que conta de 0 á 10 na mesma linha esperando 1s entre cada numero

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Contando até Dez, ");
+
+Thread.Sleep(1000); Console.Write("0, ");
+Thread.Sleep(1000); Console.Write("1, ");
+Thread.Sleep(1000); Console.Write("2, ");
+Thread.Sleep(1000); Console.Write("3, ");
+Thread.Sleep(1000); Console.Write("4, ");
+Thread.Sleep(1000); Console.Write("5, ");
+Thread.Sleep(1000); Console.Write("6, ");
+Thread.Sleep(1000); Console.Write("7, ");
+Thread.Sleep(1000); Console.Write("8, ");
+Thread.Sleep(1000); Console.Write("9, ");
+Thread.Sleep(1000); Console.Write("10.");
+
+//Que lindor, muito organizado avise!!
